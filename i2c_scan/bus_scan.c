7457f4c6f6726296fa77ec0685a06b9094588291
ConnerParker42 <copa5633@colorado.edu>
2025-02-07 10:51:22 -0700

    adding build files

--- i2c_scan/bus_scan.c
+++ i2c_scan/bus_scan.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/binary_info.h"
 #include "hardware/i2c.h"
 
 // I2C reserves some addresses for special purposes. We exclude these from the scan.
@@ -33,6 +35,7 @@ void pico_set_led(bool led_on) {
 
 
 int main() {
+    // Enable UART so we can print status output
     stdio_init_all();
 #if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
 #warning i2c/bus_scan example requires a board with I2C pins
@@ -44,37 +47,36 @@ int main() {
     gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
     gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
     gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
-    int rc = pico_led_init();
     // Make the I2C pins available to picotool
     bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+    while(true){
+        printf("\nI2C Bus Scan\n");
+        printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
 
-    pico_set_led(true);
+        for (int addr = 0; addr < (1 << 7); ++addr) {
+            if (addr % 16 == 0) {
+                printf("%02x ", addr);
+            }
 
-    printf("\nI2C Bus Scan\n");
-    printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+            // Perform a 1-byte dummy read from the probe address. If a slave
+            // acknowledges this address, the function returns the number of bytes
+            // transferred. If the address byte is ignored, the function returns
+            // -1.
 
-    for (int addr = 0; addr < (1 << 7); ++addr) {
-        if (addr % 16 == 0) {
-            printf("%02x ", addr);
+            // Skip over any reserved addresses.
+            int ret;
+            uint8_t rxdata;
+            if (reserved_addr(addr))
+                ret = PICO_ERROR_GENERIC;
+            else
+                ret = i2c_read_blocking(i2c_default, addr, &rxdata, 1, false);
+
+            printf(ret < 0 ? "." : "@");
+            printf(addr % 16 == 15 ? "\n" : "  ");
         }
-
-        // Perform a 1-byte dummy read from the probe address. If a slave
-        // acknowledges this address, the function returns the number of bytes
-        // transferred. If the address byte is ignored, the function returns
-        // -1.
-
-        // Skip over any reserved addresses.
-        int ret;
-        uint8_t rxdata;
-        if (reserved_addr(addr))
-            ret = PICO_ERROR_GENERIC;
-        else
-            ret = i2c_read_blocking(i2c_default, addr, &rxdata, 1, false);
-
-        printf(ret < 0 ? "." : "@");
-        printf(addr % 16 == 15 ? "\n" : "  ");
+        printf("Done.\n");
     }
-    printf("Done.\n");
-
     return 0;
+#endif
 }
+
