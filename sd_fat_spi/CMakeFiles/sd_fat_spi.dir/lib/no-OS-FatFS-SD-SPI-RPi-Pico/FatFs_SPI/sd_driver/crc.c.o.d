f2766eb82fe5bd70cee6275b96df6e9af128720f
ConnerParker42 <copa5633@colorado.edu>
2025-02-07 11:05:22 -0700

    changing cmake

--- sd_fat_spi/CMakeFiles/sd_fat_spi.dir/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/crc.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/sd_fat_spi.dir/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/crc.c.o: \
- /home/connerparker/ASEN_4013/AERIS-Software/sd_fat_spi/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/crc.c \
- /home/connerparker/ASEN_4013/AERIS-Software/sd_fat_spi/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/crc.h \
- /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h
