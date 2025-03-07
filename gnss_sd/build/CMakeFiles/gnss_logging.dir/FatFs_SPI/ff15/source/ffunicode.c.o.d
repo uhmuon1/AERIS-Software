e207cdc69b26d8b7c9349148f5e6f369a5733f32
Joshua <jkbernst@gmail.com>
2025-03-07 14:44:53 -0700

    removed unnecessary build and cleaned up file

--- gnss_sd/build/CMakeFiles/gnss_logging.dir/FatFs_SPI/ff15/source/ffunicode.c.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/gnss_logging.dir/FatFs_SPI/ff15/source/ffunicode.c.o: \
- /home/jkbernst/Senior_Projects/AERIS-Software/gnss_sd/FatFs_SPI/ff15/source/ffunicode.c \
- /home/jkbernst/Senior_Projects/AERIS-Software/gnss_sd/FatFs_SPI/ff15/source/ff.h \
- /home/jkbernst/Senior_Projects/AERIS-Software/gnss_sd/FatFs_SPI/ff15/source/ffconf.h \
- /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h
--- gnss_sd/build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/home/jkbernst/Senior_Projects/AERIS-Software/gnss_sd/build/_deps/picotool/"
-};
