1db174661fd192f1cbbf126cdce2fdebc696b9f1
ConnerParker42 <copa5633@colorado.edu>
2025-03-16 11:38:37 -0600

    adding motor stuff

---
+++ motor_test_c/build/pico-sdk/src/rp2040/boot_stage2/CMakeFiles/bs2_default_library.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/connerparker/ASEN_4013/AERIS-Software/motor_test_c/build/pico-sdk/src/rp2040/boot_stage2/bs2_default_padded_checksummed.S" "/home/connerparker/ASEN_4013/AERIS-Software/motor_test_c/build/pico-sdk/src/rp2040/boot_stage2/CMakeFiles/bs2_default_library.dir/bs2_default_padded_checksummed.S.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/home/connerparker/ASEN_4013/AERIS-Software/motor_test_c/$(PICO_SDK_PATH)/src"
+  "/home/connerparker/ASEN_4013/AERIS-Software/motor_test_c/("
+  "/home/connerparker/ASEN_4013/AERIS-Software/pico-sdk"
+  "/home/connerparker/ASEN_4013/AERIS-Software/motor_test_c/)"
+  "/src/rp2_common/hardware_i2c/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
