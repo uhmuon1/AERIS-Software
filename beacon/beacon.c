fdaa2b81cb2124bfc385f412411731dcf96088ed
Joshua <jkbernst@gmail.com>
2025-03-31 13:10:04 -0600

    updates to beacon code

--- beacon/beacon.c
+++ beacon/beacon.c
@@ -1,20 +1,27 @@
 #include "pico/stdlib.h"
 
-#define SIGNAL_PIN 16  // GPIO pin, can change
+#define SIGNAL_PIN 22  // GPIO pin, can change
 
 int main() {
     stdio_init_all();
 
     gpio_init(SIGNAL_PIN);
+    gpio_init(25);
     gpio_set_dir(SIGNAL_PIN, GPIO_OUT);
+    gpio_set_dir(25, GPIO_OUT);
 
     // TODO for main code: conditional to turn beacon on
-    // Send high signal
-    gpio_put(SIGNAL_PIN, 1);
-    sleep_ms(1000);
+    while (true) {
+        // Send high signal
+        gpio_put(SIGNAL_PIN, 1);
+        gpio_put(25,1);
+        sleep_ms(15000);
 
-    // Set back to low
-    gpio_put(SIGNAL_PIN, 0);
+        // Set back to low
+        gpio_put(SIGNAL_PIN, 0);
+        gpio_put(25,0);
+        sleep_ms(15000);
 
+    }
     return 0;
 }
