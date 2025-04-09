991ba1f0e208fa32d57189cabf2603a11d5a9bc9
ConnerParker42 <copa5633@colorado.edu>
2025-04-09 14:12:42 -0600

    remove build files

--- flight_code/build/CMakeFiles/flight_code.dir/FatFs_SPI/sd_driver/crc.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/flight_code.dir/FatFs_SPI/sd_driver/crc.c.o: \
- /home/connerparker/ASEN_4013/AERIS-Software/flight_code/FatFs_SPI/sd_driver/crc.c \
- /home/connerparker/ASEN_4013/AERIS-Software/flight_code/FatFs_SPI/sd_driver/crc.h \
- /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h
