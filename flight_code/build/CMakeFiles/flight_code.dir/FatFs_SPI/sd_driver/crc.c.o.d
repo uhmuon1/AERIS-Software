2a49dc0f56e408e6c9d6479f172c389bb6521c86
uhmuon1 <thco6316@colorado.edu>
2025-04-17 15:03:46 -0600

    fixed buff

--- flight_code/build/CMakeFiles/flight_code.dir/FatFs_SPI/sd_driver/crc.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/flight_code.dir/FatFs_SPI/sd_driver/crc.c.o: \
- /Users/tommycollier/Downloads/AERIS-Software/flight_code/FatFs_SPI/sd_driver/crc.c \
- /Users/tommycollier/Downloads/AERIS-Software/flight_code/FatFs_SPI/sd_driver/crc.h \
- /Applications/ArmGNUToolchain/14.2.rel1/arm-none-eabi/lib/gcc/arm-none-eabi/14.2.1/include/stddef.h
